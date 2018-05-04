f1d6f65f6b75f2eac5c14ea7aca7c0ce508d43b4
Sergey Ankinovich <codeavr@ya.ru>
2018-05-04 23:08:51 +0300

    Added AplusB

---
+++ AplusB/AplusB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Application
+{
+	class MainClass
+	{
+		public static void Main(string[] args){
+			
+			string[]data=Console.ReadLine().Split (' ');
+			int M = int.Parse(data [0]);
+			int N = int.Parse(data [1]);
+			Console.WriteLine (M+N);
+
+		}
+	}
+}
