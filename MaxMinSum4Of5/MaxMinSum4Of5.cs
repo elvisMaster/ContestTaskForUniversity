8846c51e3ed0f61f236608bcbd57a5505359011b
Евгений Жмакин <crashik_96@mail.ru>
2018-05-05 20:35:36 +0300

    Done

---
+++ MaxMinSum4Of5/MaxMinSum4Of5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            Array.Sort(arr);
+            Int64 op1 = 0, op2 = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                op1 += arr[i];
+                op2 += arr[4 - i];
+            }
+            Console.WriteLine(op1 + " " + op2);
+        }
+    }
+}
