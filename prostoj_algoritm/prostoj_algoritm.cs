bef129a67f75dfb9c1a2d9484398b866423109b4
uncanny25 <arman_barakazyan@outlook.com>
2018-05-07 20:52:36 +0300

    Added 13th task

---
+++ prostoj_algoritm/prostoj_algoritm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            if (((a + 100) / (a * 2)) > ((a * 3) / (a + 2)))
+            {
+                Console.WriteLine("YES");
+            }
+            else Console.WriteLine("NO");
+            Console.Read();
+        }
+    }
+}
