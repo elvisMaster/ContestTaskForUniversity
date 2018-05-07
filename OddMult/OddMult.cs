5e03d9cb5567a7d0022eab1c044b455e6c8230a4
Евгений Жмакин <crashik_96@mail.ru>
2018-05-07 17:20:25 +0300

    Fixed NumStringInverse.zip and added new task

---
+++ OddMult/OddMult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int mult = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 1)
+                {
+                    if (mult == 0)
+                        mult = arr[i];
+                    else
+                        mult *= arr[i];
+                }
+            }
+
+            Console.WriteLine(mult);
+        }
+    }
+}
