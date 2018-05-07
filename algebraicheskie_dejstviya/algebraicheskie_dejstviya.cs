6adc9eabc3d800c43f34f14213b5132e0966b755
uncanny25 <arman_barakazyan@outlook.com>
2018-05-07 20:28:41 +0300

    Added 12th task

---
+++ algebraicheskie_dejstviya/algebraicheskie_dejstviya.cs
@@ -0,0 +1,28 @@
+﻿namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            int k = int.Parse(data[1]);
+            switch (k)
+            {
+                case 1:
+                    Console.WriteLine((a*a).ToString());
+                    break;
+                case 2:
+                    Console.WriteLine((a * a * a).ToString());
+                    break;
+                case 3:
+                    Console.WriteLine((a * 2).ToString());
+                    break;
+                case 4:
+                    Console.WriteLine((a * 3).ToString());
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
