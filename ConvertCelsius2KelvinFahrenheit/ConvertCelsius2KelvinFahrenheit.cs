6adf708cc60ea19d906f4571aba3dfd8782b32d0
ashrafulrabbi <ashrafulrabbi@yahoo.com>
2018-05-08 00:24:47 +0300

    Added ConvertCelsius2KelvinFahrenheit task

---
+++ ConvertCelsius2KelvinFahrenheit/ConvertCelsius2KelvinFahrenheit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertCelsius2KelvinFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("{0} {1}", celsius + 273, celsius * 18 / 10 + 32);
+        }
+    }
+}
