61c6509eb5e352092bda54197acd924bcbc6ec2f
Евгений Жмакин <crashik_96@mail.ru>
2018-05-07 16:42:05 +0300

    Deleted MaxMinSum4Of5 and added new task

---
+++ NumStringInverse/NumStringInverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            string result = "";
+            for (int i = 0; i < number.Length; i++)
+            {
+                result += number[number.Length - 1 - i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
