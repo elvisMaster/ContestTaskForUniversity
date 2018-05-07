71a1b41247c54d493f0b16885a88030a029d7e9c
uncanny25 <arman_barakazyan@outlook.com>
2018-05-07 16:46:06 +0300

    Added 7th task

---
+++ udvoenie_peremennoj_n_raz/udvoenie_peremennoj_n_raz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int a = int.Parse(data[0]);
+            int n = int.Parse(data[1]);
+            for (int i = 0; i < n; i++)
+            {
+                a = a * 2;
+            }
+            Console.WriteLine(a.ToString());
+            Console.Read();
+        }
+    }
+}
