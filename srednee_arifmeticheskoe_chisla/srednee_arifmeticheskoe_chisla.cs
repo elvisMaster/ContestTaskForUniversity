1b4f2f20bce7e58167035c36438a68f57e4fc452
uncanny25 <arman_barakazyan@outlook.com>
2018-05-07 19:25:04 +0300

    Added 9th task

---
+++ srednee_arifmeticheskoe_chisla/srednee_arifmeticheskoe_chisla.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int length = data[0].Length;
+            int[] b = new int[length];
+            int _i = 0;
+            foreach (char c in data[0])
+            {
+                b[_i] = int.Parse(c.ToString());
+                _i++;
+            }
+            string result="";
+            if (length <11)
+            {
+                int sum = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    sum += b[i];
+                }
+                result = (Convert.ToDouble(sum)/length).ToString();
+            }
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
