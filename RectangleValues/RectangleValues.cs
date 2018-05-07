23cc10e7e665540544951551e19df9350489a15a
Евгений Жмакин <crashik_96@mail.ru>
2018-05-07 18:15:13 +0300

    Added task RectangleValues

---
+++ RectangleValues/RectangleValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            double a = input[0];
+            double b = input[1];
+
+            Console.WriteLine("{0} {1}", a * b, (a + b) * 2);
+        }
+    }
+}
