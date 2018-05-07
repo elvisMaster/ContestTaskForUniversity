86613401f4a591907c22555ed9fd00cb1603a66d
Евгений Жмакин <crashik_96@mail.ru>
2018-05-07 18:02:17 +0300

    Added task AbsValueCompare

---
+++ AbsValueCompare/AbsValueCompare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                if (Math.Abs(arr[i]) > 50)
+                    count++;
+
+            Console.WriteLine(count);
+        }
+    }
+}
