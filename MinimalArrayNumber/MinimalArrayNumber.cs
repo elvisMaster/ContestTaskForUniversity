352cc024402bc0384109cdf7c20b10c20896d71a
Евгений Жмакин <crashik_96@mail.ru>
2018-05-07 18:31:03 +0300

    Added task MinimalArrayNumber

---
+++ MinimalArrayNumber/MinimalArrayNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToDouble(arrTemp));
+
+            double index = 0;
+            double min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var tmp = Math.Abs(arr[i]);
+                if (tmp < min)
+                {
+                    index = i;
+                    min = tmp;
+                }
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
