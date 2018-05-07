05e0dfb2ce5b95c909d48ae147e77c5161bc677c
Евгений Жмакин <crashik_96@mail.ru>
2018-05-07 17:45:20 +0300

    Added CalculateFunction task

---
+++ CalculateFunction/CalculateFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = double.Parse(Console.ReadLine());
+            double y = int.MaxValue;
+
+            if (x > 0)
+                y = 2 * x - 10;
+            else if (x == 0)
+                y = 0;
+            else if (x < 0)
+                y = 2 * Math.Abs(x) - 1;
+
+            Console.WriteLine(y);
+        }
+    }
+}
