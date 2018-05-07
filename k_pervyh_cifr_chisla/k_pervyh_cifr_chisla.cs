80840f25f8b7e49920f7d7cdf7f92d902a93abbe
uncanny25 <arman_barakazyan@outlook.com>
2018-05-07 19:49:24 +0300

    Added 10th task

---
+++ k_pervyh_cifr_chisla/k_pervyh_cifr_chisla.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ');
+            int length = data[0].Length;
+            int k = int.Parse(data[1]);
+            int[] b = new int[length];
+            int _i = 0;
+            foreach (char c in data[0])
+            {
+                b[_i] = int.Parse(c.ToString());
+                _i++;
+            }
+            string result = "";
+            if (k <= length && length<11)
+            {
+                int[] b_k = new int[k];
+                Array.Copy(b, b_k, k);
+                for (int i = 0; i < k; i++)
+                {
+                    result += b_k[i].ToString();
+                }
+            }
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
