91481ee18d083a87a9a5ba3873eef0c33ac29b2d
ashrafulrabbi <ashrafulrabbi@yahoo.com>
2018-05-08 08:08:27 +0300

    added

---
+++ calculateFactorial/calculateFactorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace calculateFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f = 1, num;
+            
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+                f = f * i;
+
+            Console.WriteLine("{0}", f);
+            Console.Read();
+        }
+    }
+}
