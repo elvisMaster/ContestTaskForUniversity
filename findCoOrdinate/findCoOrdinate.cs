d615f382aed44878abc2257dc53c57dfc905cdb6
ashrafulrabbi <ashrafulrabbi@yahoo.com>
2018-05-08 10:39:19 +0300

    added

---
+++ findCoOrdinate/findCoOrdinate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace findCoOrdinate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] data = Console.ReadLine().Split(' ');
+            int co1 = int.Parse(data[0]);
+            int co2 = int.Parse(data[1]);
+
+            if (co1 > 0 && co2 > 0)
+                Console.Write("1");
+            else if (co1 < 0 && co2 > 0)
+                Console.Write("2");
+            else if (co1 < 0 && co2 < 0)
+                Console.Write("3");
+            else if (co1 > 0 && co2 < 0)
+                Console.Write("4");
+            else if (co1 == 0 && co2 == 0)
+                Console.Write("origin", co1, co2);
+            else if (co1 == 0 && co2 != 0)
+                Console.Write("Y");
+            else if (co1 != 0 && co2 == 0)
+                Console.Write("X");
+            Console.ReadLine();
+        }
+    }
+}
