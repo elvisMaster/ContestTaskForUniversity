18567643bc67f1c55e4e31d8575b95092a15c020
Sergey Ankinovich <codeavr@ya.ru>
2018-05-08 10:50:48 +0300

    Added IsPrimeNumber (2)

---
+++ IsPrimeNumber/IsPrimeNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int n)
+    {
+        if (n < 2) 
+        {
+            return false;
+        }
+
+        for (int i = 2, limit = (int)Math.Ceiling(Math.Sqrt(n)); i <= limit; i++)
+        {
+            if (n % i == 0 && n != i)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(IsPrime(n) ? "+" : "-");
+    }
+}
