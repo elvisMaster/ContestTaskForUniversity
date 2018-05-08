8ba3b2706f15f82fe58abb52f53bb6530a151be7
ashrafulrabbi <ashrafulrabbi@yahoo.com>
2018-05-08 09:20:52 +0300

    added

---
+++ rectangularTriangle/rectangularTriangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace rectangularTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows, k = 1;
+
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", k++);
+                Console.WriteLine("\n");
+                
+            }
+        }
+    }
+}
